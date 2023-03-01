d4512b98fb05153ae517b489a1c7ddabc692b1b9
Bianca Galindo <bianca.yamileth.galindo@outlook.com>
2023-02-28 21:31:15 -0600

    Mesaje nuevo

--- src/pruebaGit/prueba.java
+++ src/pruebaGit/prueba.java
@@ -5,6 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
 	      System.out.println("git");
 	      System.out.println("Bianca");
+	      System.out.println("Commit");
 	}
 
 }
